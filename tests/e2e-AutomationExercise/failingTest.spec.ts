70764bebc4145c4059b5bb1a23757f16db910882
jankovic-ivan <iv.jankovic.an@gmail.com>
2025-12-09 13:46:44 +0100

    Cleanup after verification of workflow

--- tests/e2e-AutomationExercise/failingTest.spec.ts
+++
@@ -1,6 +0,0 @@
-import { test, expect } from '../../fixtures/pom/test-options';
-
-test('TC01 - Failing Sanity Test', { tag: '@UI' }, async ({ homePage }) => {
-    await homePage.navigateToHomePageUser();
-    expect(2).toEqual(3);
-});
